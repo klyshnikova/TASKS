48075f588eee5eec7f36888dca91820ea78f4f23
klyshnikova <klyshniova@mail.ru>
2023-10-18 12:26:08 +0200

    36

--- 36-random-numbers/finish.js
+++ 36-random-numbers/finish.js
@@ -17,4 +17,18 @@
 const MIN = 1000
 const MAX = 9999
 
+function randomas(min,max){
+    let randomNum = Math.floor(Math.random(MIN,MAX)* MAX)
+    return randomNum
+}
+
 const myNumbers = [2355, 7235, 8135, 1762, 2361, 8351]
+
+res = randomas(MIN,MAX)
+    if(myNumbers.some(element=>element === res)){
+        randomas(MIN,MAX)
+    }
+    else
+    myNumbers.push(res)
+console.log(res)
+console.log(myNumbers)
